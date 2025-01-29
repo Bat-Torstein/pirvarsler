b85cd83a3302601beafce8f474d8e2d8d08b8f83
Torstein Bjørgan <torsteib@gmail.com>
2025-01-29 22:01:25 +0100

    Hard-coded config for now

--- PirvarslerLib/UriBuilder.cs
+++ PirvarslerLib/UriBuilder.cs
@@ -2,10 +2,10 @@
 public class UriBuilder
 {
   private const string StringDateFormat = "yyyy-MM-dd";
-  static public string BuildUri(DateTime now, Config config)
+  static public string BuildUri(DateTime now)
   {
     var time = now.Date.AddDays(1).ToString(StringDateFormat);
  
-    return $"{config.BaseUrl}?latitude={config.Latitude}&longitude={config.Longitude}&language=nb&interval={config.Interval}&fromTime={time}&toTime={time}&referenceCode=CD&place={config.Place}";
+    return $"{Config.BaseUrl}?latitude={Config.Latitude}&longitude={Config.Longitude}&language=nb&interval={Config.Interval}&fromTime={time}&toTime={time}&referenceCode=CD&place={Config.Place}";
   }
 }
